653054d1e1d134e074e693c02add7aec8ff961d4
hongwanzhen <hongwanzhen@cmss.chinamobile.com>
2020-04-03 13:49:54 +0800

    commit 0403

--- javaStudy/src/main/java/com/hong/javacore/test/StringTest.java
+++ javaStudy/src/main/java/com/hong/javacore/test/StringTest.java
@@ -2,13 +2,16 @@ package com.hong.javacore.test;
 
 import com.hong.model.User;
 
+import java.util.UUID;
+
 public class StringTest {
     public static void main(String[] args) {
         System.out.println("dd");
         String s = "abcdefg";
         System.out.println(s.indexOf(61));
-
         System.out.println((char)61);
-
+        String randomStr = UUID.randomUUID().toString();
+        System.out.println(randomStr);
+        System.out.println(System.currentTimeMillis());
     }
 }
