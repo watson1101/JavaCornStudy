27fa446dbf1f0dfdee3440bf8e9e5f71bbb391d6
watson1101 <anye2009@aliyun.com>
2023-02-13 04:43:03 +0800

    add java app code

--- javaApp/src/Main.java
+++ javaApp/src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        subThreadGetParentThreadValue();
     }
+
+    /****** 子线程 获取父线程threadLacal的值
+     * 通过 InheritableThreadLocal 来获取
+     * ******/
+    private static void subThreadGetParentThreadValue() {
+        Thread parentParent = new Thread(() -> {
+            ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+            threadLocal.set(1);
+            InheritableThreadLocal<Integer> inheritableThreadLocal = new InheritableThreadLocal<>();
+            inheritableThreadLocal.set(2);
+
+            new Thread(() -> {
+                System.out.println("threadLocal = " + threadLocal.get());
+                System.out.println("inheritableThreadLocals = " + inheritableThreadLocal.get());
+            }).start();
+        }, "父线程");
+        parentParent.start();
+    }
+
 }
