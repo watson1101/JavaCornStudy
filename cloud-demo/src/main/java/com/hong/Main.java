b28025533ba1a7a81c20708d9928616d6f1cc4ca
watson1101 <anye2009@aliyun.com>
2023-02-17 04:43:35 +0800

    nacos-config demo,run success

---
+++ cloud-demo/src/main/java/com/hong/Main.java
@@ -0,0 +1,7 @@
+package com.hong;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ nacos-config/src/test/java/com/hong/nacosconfig/NacosConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hong.nacosconfig;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NacosConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
