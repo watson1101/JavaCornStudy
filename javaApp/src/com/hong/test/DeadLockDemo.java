05229723970b1287aa4533e67a01dba6209eb76b
watson1101 <anye2009@aliyun.com>
2023-03-20 00:05:33 +0800

    dead lock demo

---
+++ javaApp/src/com/hong/test/DeadLockDemo.java
@@ -0,0 +1,37 @@
+package com.hong.test;
+
+public class DeadLockDemo  {
+    static Object a = new Object();
+    static Object b = new Object();
+
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (a) {
+                    System.out.println("a is get lock");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                    e.printStackTrace();}
+                }
+                synchronized (b) {
+                }
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (b) {
+                    System.out.println("b is get lock");
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e){
+                    e.printStackTrace();}
+                }
+                synchronized (a) {
+                }
+            }
+        }).start();
+    }
+}
