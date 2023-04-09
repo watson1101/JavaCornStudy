672879429c658b775e8a6fd2613a04c892e103e3
watson1101 <anye2009@aliyun.com>
2023-04-10 02:33:46 +0800

    静态代理和动态代理demo

--- javaApp/src/com/hong/test/core/proxy/RobotHandle.java
+++ javaApp/src/com/hong/test/core/proxy/RobotHandle.java
@@ -9,11 +9,21 @@ public class RobotHandle implements InvocationHandler {
 
     public Object getProxyInstance(Object target) {
         this.proxyTarget = target;
-        // 参数分别为 被代理类的类加载器，被代理类的接口，被代理类的处理器
+        // 参数分别为
+        // 1.被代理类的类加载器；
+        // 2.被代理类的接口；
+        // 3.代理类本身，即告诉代理类，代理类遇到某个委托类的方法时该调用哪个类下的invoke方法
         return Proxy.newProxyInstance(proxyTarget.getClass().getClassLoader(), proxyTarget.getClass().getInterfaces(), this);
     }
 
-
+    /**
+     * 第一个参数为 Proxy 类类型实例，如匿名的 $proxy 实例
+     * 第二个参数为委托类的方法对象
+     * 第三个参数为委托类的方法参数
+     * 返回类型为委托类某个方法的执行结果
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
         Object methodObject = null;
