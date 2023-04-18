91e1bb1c02b9cbe1d647ba30da373b0b4f4ac488
watson1101 <anye2009@aliyun.com>
2023-04-19 01:00:53 +0800

    流处理，翻转链表

--- javaApp/src/com/hong/test/core/nodeDemo/NodeDemo.java
+++ javaApp/src/com/hong/test/core/nodeDemo/NodeDemo.java
@@ -26,9 +26,11 @@ linkedListReverse();
             System.out.print(h.getValue() + " ");
             h = h.getNext();
         }
-        // 调用反转方法
-        head = reverse1(head);
-
+        // 调用递归反转方法
+//        head = reverse1(head);
+        // 调用遍历反转方法
+        head = reverse2(head);
+        //
         System.out.println("\n**************************");
         // 打印反转后的结果
         while (null != head) {
@@ -52,6 +54,29 @@ linkedListReverse();
         return reHead;// 反转后新链表的头结点
     }
 
+    /**
+     * 遍历，将当前节点的下一个节点缓存后更改当前节点指针
+     * @param head
+     * @return
+     */
+    public static Node reverse2(Node head) {
+        if (head == null)
+        {return head;}
+        Node pre = head;// 上一结点
+        Node cur = head.getNext();// 当前结点
+        Node tmp;// 临时结点，用于保存当前结点的指针域（即下一结点）
+        while (cur != null) {// 当前结点为null，说明位于尾结点
+            tmp = cur.getNext();
+            cur.setNext(pre);// 反转指针域的指向
+
+            // 指针往下移动
+            pre = cur;
+            cur = tmp;
+        }
+        // 最后将原链表的头节点的指针域置为null，还回新链表的头结点，即原链表的尾结点
+        head.setNext(null);
+        return pre;
+    }
 
 
 }
