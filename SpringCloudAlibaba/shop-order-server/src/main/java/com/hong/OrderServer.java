57c1803c4cb1216f03633e3a04e7749f8dadba37
watson1101 <anye2009@aliyun.com>
2023-05-04 02:33:32 +0800

    服务注册到 nacos

--- SpringCloudAlibaba/shop-order-server/src/main/java/com/hong/OrderServer.java
+++ SpringCloudAlibaba/shop-order-server/src/main/java/com/hong/OrderServer.java
@@ -3,11 +3,14 @@ package com.hong;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @MapperScan("com.hong.mapper")
+// nacos注册中心
+@EnableDiscoveryClient
 public class OrderServer {
 
     /**
--- SpringCloudAlibaba/shop-product-server/src/main/java/com/hong/ProductServer.java
+++ SpringCloudAlibaba/shop-product-server/src/main/java/com/hong/ProductServer.java
@@ -3,9 +3,12 @@ package com.hong;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.hong.mapper")
+// nacos注册中心
+@EnableDiscoveryClient
 public class ProductServer {
     public static void main(String[] args) {
         SpringApplication.run(ProductServer.class, args);
